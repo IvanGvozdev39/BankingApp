b9e43f1b11afffecc8e6ae983bb99261b1fece32
Ivan Gvozdev <gvozde39@gmail.com>
2024-07-01 14:15:15 +0300

    Optimized bottom sheets and dialogs (smoother dismissal)

--- app/src/main/java/com/test/bankingapp/transaction/presentation/util/CustomDatePickerDialog.kt
+++ app/src/main/java/com/test/bankingapp/transaction/presentation/util/CustomDatePickerDialog.kt
@@ -1,15 +1,27 @@
 package com.test.bankingapp.transaction.presentation.util
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -36,9 +48,13 @@ fun CustomDatePickerDialog(
     if (datePickerExpanded) {
         BackHandler {
             datePickerExpanded = false
+            onDialogDismissed()
         }
 
-        Dialog(onDismissRequest = { datePickerExpanded = false }) {
+        Dialog(onDismissRequest = {
+            datePickerExpanded = false
+            onDialogDismissed()
+        }) {
             Card(
                 shape = RoundedCornerShape(16.dp),
                 backgroundColor = colorResource(id = R.color.dark_gray)
